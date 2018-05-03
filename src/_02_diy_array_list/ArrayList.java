79b1796f79744d92dd4af7cdfd746866d7580a48
Chloe Luwa <chloeluwa@yahoo.com>
2018-05-02 17:12:38 -0700

    ArrayList, CustomArrayListTest
    ArrayList, CustomArrayListTest

--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -1,97 +1,83 @@
 package _02_diy_array_list;
 
-import java.lang.reflect.Array;
-
-public class ArrayList<V>{
+public class ArrayList<V> {
 
 	V[] list;
-	
-	public ArrayList(){
+
+	public ArrayList() {
 		list = (V[]) new Object[0];
 	}
+
 	V[] newlist;
+
 	public void add(V value) {
 
-		
-		newlist = (V[]) new Object[list.length +1];
-		for(int i = 0; i < list.length; i++) {
-			
+		newlist = (V[]) new Object[list.length + 1];
+		for (int i = 0; i < list.length; i++) {
+
 			newlist[i] = list[i];
-		
+
 		}
-		
+
 		newlist[list.length] = value;
 		list = newlist;
 	}
-	
+
 	public void remove(int l) {
 		newlist = (V[]) new Object[list.length - 1];
-		for(int k = 0; k < list.length - 1; k++) {
-			if(k < l) {
+		for (int k = 0; k < list.length - 1; k++) {
+			if (k < l) {
 				newlist[k] = list[k];
-			}
-			else if(k >=l) {
-				newlist[k] = list[k+1];
-			}
-			else {
-			
+			} else if (k >= l) {
+				newlist[k] = list[k + 1];
+			} else {
+
 			}
 		}
-	}
-	
-	
-	
-	public V get(int l) {
-		
-		return list[l];
-	}
-	
-	
-	
-	
-	public void addAt(int l, V value) {
-		newlist = (V[]) new Object[list.length +1];
-	for(int j = 0; j < newlist.length; j++) {
-		if(j < l) {
-			newlist[j] = list[j];
-		}
-		else if(j == l) {
-			newlist[j] = value;
-		}
-		else if(j > l) {
-			newlist[j] = list[j-1];
-		}
-	
+		list = newlist;
 	}
 
-	list = newlist;
-	
+	public V get(int l) {
+
+		return list[l];
 	}
-	
-	
-	
-	
+
+	public void addAt(int l, V value) {
+		newlist = (V[]) new Object[list.length + 1];
+		for (int j = 0; j < newlist.length; j++) {
+			if (j < l) {
+				newlist[j] = list[j];
+			} else if (j == l) {
+				newlist[j] = value;
+			} else if (j > l) {
+				newlist[j] = list[j - 1];
+			}
+
+		}
+
+		list = newlist;
+
+	}
+
 	public void set(int l, V value) {
 		newlist = (V[]) new Object[list.length];
-		for(int n = 0; n < list.length; n++) {
-			if(n == l) {
+		for (int n = 0; n < list.length; n++) {
+			if (n == l) {
 				newlist[n] = value;
-			}
-			else{
+			} else {
 				newlist[n] = list[n];
 			}
 		}
 		list = newlist;
 	}
-	
+
 	public boolean contains(V value) {
-	for(int m = 0; m < list.length; m++) {
-		if(list[m] == value) {
-			return true;
+		for (int m = 0; m < list.length; m++) {
+			if (list[m] == value) {
+				return true;
+			}
 		}
-	}
-	
-	
-	return false;
+
+		return false;
 	}
 }
